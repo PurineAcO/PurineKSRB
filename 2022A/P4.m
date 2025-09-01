661e2d02f04da1ed5efc70dc411214b65001ac49
Purine <3223145991@qq.com>
2025-09-01 21:14:35 +0800

    2022A 题解好了

--- 2022A/P4.m
+++ 2022A/P4.m
@@ -1,8 +1,6 @@
-% ������֪�̶�����
-   
-% ��ֵȫ�ֱ���
-me = 1091.099;       
-m1 = 2433;       
+%% define consts
+me = 1091.099;          % 垂荡附加质量
+m1 = 2433;              % 振子质量
 m2 = 4866;
 f0 = 1760;       
 omega = 1.9806;    
@@ -18,12 +16,12 @@ MCL = 1655.909;
 M3 = 8890.7;       
 MK = 250000;      
 
-% ������������ͷ�Χ
-nvars = 2; % C��MC��������
-lb = [0, 0]; % �½�
-ub = [100000, 100000]; % �Ͻ�
+%% define GA
+nvars = 2; 
+lb = [0, 0]; 
+ub = [100000, 100000]; 
 
-% ����GA�Ż�
+
 options = optimoptions('ga', ...
     'PopulationSize', 50, ...
     'MaxGenerations', 50, ...
@@ -32,72 +30,62 @@ options = optimoptions('ga', ...
 
 [x_opt, fval_opt] = ga(@objfun, nvars, [], [], [], [], lb, ub, [], options);
 
-% ��ʾ���
+
 fprintf('���Ų�����C = %.1f, MC = %.1f\n', x_opt(1), x_opt(2));
 fprintf('���P_bar = %.4f\n', -fval_opt);
 
-% �������Ų����µĹ�������
+
 plot_optimal_power(x_opt);
 
-% ����Ŀ�꺯������Ӧ�Ⱥ����������ظ���P_bar����ΪgaĬ����С��
+%% define the J
 function fitness = objfun(x)
     C = x(1);
     MC = x(2);
     omega = 1.9806;   
-    % ��ֵ����
     initial_conditions = zeros(8, 1);
     
-    % ���ʱ�䷶Χ
     t_end_total = 200*pi/omega;
     tspan = [0, t_end_total];  
     
-    % �����ѡ��
     options = odeset(...
         'RelTol', 1e-6, ...
         'AbsTol', 1e-8, ...
         'MaxStep', 0.001 ...
     );
     
-    % ����ode45�����
     [t, sol] = ode45(@(t, y) system_equations(t, y, C, MC), tspan, initial_conditions, options);
     
-    % ��ȡ���
     zdot1 = sol(:, 5);
     zdot2 = sol(:, 6);
     thetadot1 = sol(:, 7);
     thetadot2 = sol(:, 8);
     
-    % �����������
     t_start = 100*pi/omega;
     t_end = 200*pi/omega;
-    
-    % �ҵ�������������
+
     idx_start = find(t >= t_start, 1, 'first');
     idx_end = find(t <= t_end, 1, 'last');
-    
-    % �����߽����
+
     if isempty(idx_start) || isempty(idx_end) || idx_start > idx_end
-        fitness = -1e-9; % ��Сֵ���������
+        fitness = -1e-9; 
         return;
     end
     
-    % ��ȡ����������
     zdot1_interval = zdot1(idx_start:idx_end);
     zdot2_interval = zdot2(idx_start:idx_end);
     thetadot1_interval = thetadot1(idx_start:idx_end);
     thetadot2_interval = thetadot2(idx_start:idx_end);
     t_interval = t(idx_start:idx_end);
     
-    % ����P_bar��ʱ���Ȩ���֣�
     P_vals = C * (zdot1_interval - zdot2_interval).^2 + MC * (thetadot1_interval - thetadot2_interval).^2;
     P_bar = trapz(t_interval, P_vals) / (t_end - t_start);
     
-    fitness = -P_bar; % GAĬ����С�������Է��ظ���P_bar�����
+    fitness = -P_bar; 
 end
 
-% ϵͳ΢�ַ��̣��ڲ�ʹ�ã������ⲿ�̶�������
+%% define the solver
 function dydt = system_equations(t, y, C, MC)
-    % �ⲿ�̶�����
+    % define some consts
     me = 1091.099;       
     m1 = 2433;       
     m2 = 4866;
@@ -115,7 +103,6 @@ function dydt = system_equations(t, y, C, MC)
     M3 = 8890.7;       
     MK = 250000;   
     
-    % ״̬����
     z1 = y(1);
     z2 = y(2);
     theta1 = y(3);
@@ -125,34 +112,32 @@ function dydt = system_equations(t, y, C, MC)
     thetadot1 = y(7);
     thetadot2 = y(8);
     
-    % ����I1
+    % equ (5)
     I1 = 202.75 + 2433 * (0.75 + z1 - z2)^2;
     
-    % ���Ӵ���ͨ��
+    % equ (1)
     zddot1 = (-K*(z1 - z2) - C*(zdot1 - zdot2)) / m1;
     
-    % ���Ӵ���ͨ��
+    % equ (2)
     zddot2 = (f0*cos(omega*t) - CL*zdot2 - rho*g*A*z2 - m1*zddot1) / (m2 + me);
     
-    % ������תͨ��
+    % equ (3)
     thetaddot1 = (-MC*(thetadot1 - thetadot2) - MK*(theta1 - theta2)) / I1;
     
-    % ������תͨ��
+    % equ (4)
     thetaddot2 = (M0*cos(omega*t) - MCL*thetadot2 - M3*theta2 - I1*thetaddot1) / (I2 + Ie);
     
-    % ����һ��΢�ַ�����
+    % DDD
     dydt = [
-        zdot1;               % z1��һ�׵���
-        zdot2;               % z2��һ�׵���
-        thetadot1;           % theta1��һ�׵���
-        thetadot2;           % theta2��һ�׵���
-        zddot1;              % z1�Ķ��׵���
-        zddot2;              % z2�Ķ��׵���
-        thetaddot1;          % theta1�Ķ��׵���
-        thetaddot2           % theta2�Ķ��׵���
+        zdot1;               
+        zdot2;               
+        thetadot1;           
+        thetadot2;          
+        zddot1;              
+        zddot2;          
+        thetaddot1;         
+        thetaddot2          
     ];
 end
-
-% ������������GA�����Ż�
 
 
