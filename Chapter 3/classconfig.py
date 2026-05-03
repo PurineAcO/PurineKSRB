a2be49a79d7d66a106b20f63460324c8dd556159
Purine <3223145991@qq.com>
2026-05-03 23:26:16 +0800

    流场初始化（Solve1）

--- Chapter 3/classconfig.py
+++ Chapter 3/classconfig.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+# define some constants
+gamma = 1.4      # gamma = Cp/Cv
+R = 287.06       # gas constant,which is for air
+T0 = 288.16      # referrence temperature,入口静温
+Ts = 110         # Sutherland costant
+mu0 = 1.7894e-5  # \mu for T0,动力粘度
+P0 = 101325.0    # referrence pressure,入口静压
+c0 = 340.28      # referrence sound velocity,海平面参考声速
+
+#define the simulation state
+AOA = 0          # attack angle(which unit is deg)
+Ma = 0.2         # Mach Number
+
+#define the farfield
+
+
+
 # area for the global variables 
 i_total = 0
 j_total = 0
@@ -22,6 +39,17 @@ class cell_class:
         self.x = 0        # cell center x
         self.y = 0        # cell center y   
         self.vol = 0      # cell volume(for 2D,it iterally means area)
+        self.rho = 0
+        self.p = 0
+        self.T = 0
+        self.u = 0
+        self.v = 0
+        self.E = 0
+        self.H = 0
+        self.c = 0
+        self.ma = 0
+        self.miu = 0
+        self.miubl = 0
 
 class face_class:
     def __init__(self,index):
---
+++ Chapter 3/initialize.py
@@ -0,0 +1,18 @@
+import classconfig as cc
+import math
+
+def initialization(T0=cc.T0,AOA=cc.AOA,Ma=cc.Ma,P0=cc.P0):
+    """init the whole fluid field,you should give the Ma"""
+    for i in range(1,cc.i_total,1):
+        for j in range(1,cc.j_total,1):
+            cc.CellList[i][j].ma = Ma
+            cc.CellList[i][j].T = T0/(1 + (cc.gamma-1)/2 * (Ma**2))
+            cc.CellList[i][j].p = P0 * (cc.CellList[i][j].T-T0)**(cc.gamma/(cc.gamma-1))
+            cc.CellList[i][j].c = math.sqrt(cc.gamma*cc.R*cc.CellList[i][j].T)
+            cc.CellList[i][j].rho = cc.CellList[i][j].p/(cc.R*cc.CellList[i][j].T)
+            cc.CellList[i][j].u = cc.CellList[i][j].c * Ma * math.cos(AOA*180/math.pi)
+            cc.CellList[i][j].v = cc.CellList[i][j].c * Ma * math.sin(AOA*180/math.pi)
+            cc.CellList[i][j].E = cc.CellList[i][j].p/(cc.CellList[i][j].rho*(cc.gamma-1))+(cc.CellList[i][j].v**2+cc.CellList[i][j].v**2)/2
+            cc.CellList[i][j].H = cc.CellList[i][j].E + cc.CellList[i][j].p/cc.CellList[i][j].rho
+            cc.CellList[i][j].miu = cc.mu0 * (cc.CellList[i][j].T/T0)**1.5 * (T0+cc.Ts)/(cc.CellList[i][j].T+cc.Ts)
+            cc.CellList[i][j].miubl = cc.CellList[i][j].miu *0.1/cc.CellList[i][j].rho
